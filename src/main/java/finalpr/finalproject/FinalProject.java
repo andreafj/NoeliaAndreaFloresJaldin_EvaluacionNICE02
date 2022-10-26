66b953611e56f94f33d0f1cc6a900bdc53048a82
andrea flores jaldin <floresjaldin@gmail.com>
2022-10-26 18:02:13 -0400

    final test

--- src/main/java/finalpr/finalproject/FinalProject.java
+++ src/main/java/finalpr/finalproject/FinalProject.java
@@ -1,5 +1,7 @@
 package finalpr.finalproject;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Andrea
@@ -7,6 +9,36 @@ package finalpr.finalproject;
 public class FinalProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! :D");
+        //System.out.println("Hello World! :D");
+        
+        String cadena = JOptionPane.showInputDialog("Digite una cadena");
+        
+   
+        //Dividimos la cadena por espacios en un array
+        String [] arrayPalabras = cadena.split("\\s+");
+        
+        String [] arrayResultado = new String[arrayPalabras.length];
+        
+        
+        //Recorremos cada palabra para buscar el inner content
+        for (int i = 0; i < arrayPalabras.length; i++) {
+            
+            //Solo si la palabra es mayor a 3 buscamos inner content
+            if(arrayPalabras[0].length()>3){
+                //Aqui conseguimos el inner content
+                String innerContent =conseguirInnerContent(arrayPalabras[0]);
+                //Aqui juntamos la palabras de nuevo es decir primer caracter y ultimo con el inner en orden descendente
+                String ordenDescendente = ordernarDescendente(innerContent,arrayPalabras[0])
+                
+            }
+            else{
+                arrayResultado[i]=arrayPalabras[i];
+            }
+        }
+        
+        
+        
     }
+    
+     
 }
