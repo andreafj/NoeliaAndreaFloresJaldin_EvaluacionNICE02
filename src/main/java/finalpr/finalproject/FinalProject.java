13ed6920f670f15dd0ce8862797326c0f65de0ea
andrea flores jaldin <floresjaldin@gmail.com>
2022-10-26 00:36:26 -0400

    Hello world with java

---
+++ src/main/java/finalpr/finalproject/FinalProject.java
@@ -0,0 +1,12 @@
+package finalpr.finalproject;
+
+/**
+ *
+ * @author Andrea
+ */
+public class FinalProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! :D");
+    }
+}
